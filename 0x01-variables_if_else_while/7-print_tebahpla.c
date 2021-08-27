3b3881c0270ab036cbf7cabef55d31dd58da53d9
Innocent <kaluinnocentokoro@gmail.com>
2021-08-27 12:48:21 -0700

    created 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - Prints the lower case alphabet in reverse
+*
+* Return: Return 0
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
