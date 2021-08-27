f6c2c5ae25da244365e9cefcc1a0f681742276a9
Innocent <kaluinnocentokoro@gmail.com>
2021-08-27 11:15:40 -0700

    created 2-print_alphabet.c file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+* main - Prints the lower case alphabet
+*
+* Return: Return 0
+*/
+int main (void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
