2ab31038b9019127eb4103a8ee4411077f874716
Innocent <kaluinnocentokoro@gmail.com>
2021-08-27 12:16:47 -0700

    created 5-print_number.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+* main - Program prints digits 0-9
+*
+* Return: return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
