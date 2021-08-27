6ad7d152dee49fc07186f382bb825f39a291429a
Innocent <kaluinnocentokoro@gmail.com>
2021-08-27 12:43:38 -0700

    created 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<ctype.h>
+/**
+* main - Program prints digits 0-9
+*
+* Return: return 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
