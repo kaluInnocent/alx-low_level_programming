6767e76819bbd97b017f59ef0ec92a597019405d
Innocent <kaluinnocentokoro@gmail.com>
2021-08-31 11:18:39 -0700

    created 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* void print_alphabet_x10 - function prints the alphabets
+* prints the alphabets in lower case in 10 lines
+*
+* Return: return 0
+*/
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	
+	_putchar('\n');
+	}}
+}
