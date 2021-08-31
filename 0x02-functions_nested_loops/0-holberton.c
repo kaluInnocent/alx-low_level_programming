cf5ab179326bdd575e3cd413d39aafb9db22d226
Innocent <kaluinnocentokoro@gmail.com>
2021-08-31 05:32:45 -0700

    created 0-holberton.c and main.h

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - function prints the text "holberton"
+*
+* Return: return 0
+*/
+int main (void)
+{
+	char alx[9] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n'};
+	unsigned int ch ;
+
+	for (ch = 0; c < sizeof(alx); ch++)
+	{
+		_putchar(alx[ch]);
+	}
+	_putchar('\n');
+	return (0);
+}
