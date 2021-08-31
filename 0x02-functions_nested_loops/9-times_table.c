06972894dee7acf1336fb132ff528feb6420b889
Innocent <kaluinnocentokoro@gmail.com>
2021-08-31 15:12:49 -0700

    created 9-times_table.c file

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* times_table - function prints 9 times table, starting with 0
+*
+* Return: returns nothing
+*/
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			_putchar(i * j);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
