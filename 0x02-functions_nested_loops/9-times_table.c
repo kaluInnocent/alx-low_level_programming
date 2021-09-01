0e96b76dca1e372cd8a6681b39c18f0c19573b10
Innocent <kaluinnocentokoro@gmail.com>
2021-08-31 18:53:37 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,16 +6,14 @@
 */
 void times_table(void)
 {
-	int i;
-	int j;
+	char i;
+	char j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			int product = i * j;
-
-			_putchar(product - '0');
+			_putchar(i * j);
 			_putchar(',');
 			_putchar(' ');
 		}
