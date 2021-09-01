2872b295a2312d7e2f2fd81174333a8429a7c510
Innocent <kaluinnocentokoro@gmail.com>
2021-08-31 18:14:38 -0700

    Holberton School, 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdlib.h>
+#include<stdio.h>
+/**
+* print_to_98 - prints natural numbers from n to 98
+* @n: parameter to be supplied
+* Return: return 0
+*/
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		printf("\n");
+	}
+	else
+		printf("%d\n", n);
+}
