7f67080fc87b9b597d8581af3c3b948c813bd249
Innocent <kaluinnocentokoro@gmail.com>
2021-09-01 14:18:58 -0700

    created 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,32 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 /**
-* main - function prints the first 98 Fibonacci numbers
-* starting with 1 and 2
-*
-* Return: return (0)
+* numLength - returns the length of string
+* @num: operand number
+* Return: number of digits
+*/
+int numLength(int num)
+{
+int length = 0;
+
+if (!num)
+{
+return (1);
+}
+while (num)
+{
+num = num / 10;
+length += 1;
+}
+return (length);
+}
+/**
+* main - prints the first 98 digits of the fibonacci series
+* Return: 0
 */
 int main(void)
 {
-	unsigned long previous = 1, current = 2, new;
-	int i = 3;
+unsigned long fib1 = 1, fib2 = 2, next, nexto = 0;
+unsigned long mx = 100000000, fib1o = 0, fib2o = 0;
+short int i = 1, initial0s;
 
-	printf("%lu, %lu, ", previous, current);
-	while (i <= 98)
-	{
-		new = previous + current;
-		if (i != 98)
-		{
-			printf("%lu, ", new);
-		}
-		else
-		{
-			printf("%lu", new);
-		}
-		previous = current;
-		current = new;
-		i++;
-	}
-	printf("\n");
-	return (0);
+while (i <= 98)
+{
+if (fib1o > 0)
+printf("%lu", fib1o);
+initial0s = numLength(mx) - 1 - numLength(fib1);
+while (fib1o > 0 && initial0s > 0)
+{
+printf("%i", 0);
+initial0s--;
+}
+printf("%lu", fib1);
+next = (fib1 + fib2) % mx;
+nexto = fib1o + fib2o + (fib1 + fib2) / mx;
+fib1 = fib2;
+fib1o = fib2o;
+fib2 = next;
+fib2o = nexto;
+if (i != 98)
+printf(", ");
+else
+printf("\n");
+i++;
+}
+return (0);
 }
