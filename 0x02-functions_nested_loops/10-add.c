95497e00d705880234becd7306a2a5b90bca7749
Innocent <kaluinnocentokoro@gmail.com>
2021-08-31 17:40:43 -0700

    created a+b file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* add - function adds two numbers
+* @a: parameter 1
+* @b: parameter 2
+* Return: return 0
+*/
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
