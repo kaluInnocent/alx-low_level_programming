2299012efc9e0ed870abcf2555ee270081cb7bd3
Innocent <kaluinnocentokoro@gmail.com>
2021-09-02 15:23:55 -0700

    updated 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,18 +6,12 @@
 */
 void print_line(int n)
 {
-	int i;
+	int i = 0;
 
-	if (n <= 0)
+	while (i < n)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (i = 0; i <= n; i++)
-		{
-			_putchar('_');
-		}
+		_putchar('_');
+		i++;
 	}
 	_putchar('\n');
 }
