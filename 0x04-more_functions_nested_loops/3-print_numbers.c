d4144e4b81463a84457439bf70e1b017da662879
Innocent <kaluinnocentokoro@gmail.com>
2021-09-02 08:56:40 -0700

    created 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_numbers -  prints the numbers from 0-9
+* followed by a newline
+* Return: return nothing
+*/
+void print_numbers(void)
+{
+	int c;
+
+	for (c >= '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+}
