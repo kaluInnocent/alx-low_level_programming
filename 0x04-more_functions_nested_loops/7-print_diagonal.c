2fbfead3c3e05c5e053bfed90606f9c8f8822aaf
Innocent <kaluinnocentokoro@gmail.com>
2021-09-02 17:42:29 -0700

    updated 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,17 +14,18 @@ void print_diagonal(int n)
 		{
 			for (j = 0; j <= i; j++)
 			{
-				if (j == i)
+				if (i == j)
 				{
 					_putchar('\\');
+					_putchar('\n');
 				}
 				else
 				{
 					_putchar(' ');
 				}
 			}
-			_putchar('\n');
 		}
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
