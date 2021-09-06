aa61ece4383f59944ef055699800c2c5d55e9d20
Innocent <kaluinnocentokoro@gmail.com>
2021-09-06 12:27:09 -0700

    corrected kernel style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
 * reset_to_98 - function takes a pointer to an int
 * as parameter and duplicates the value it points to 98
-* @*n: the pointer variable
+* @n: the pointer variable
 * Return: return nothing
 */
 void reset_to_98(int *n)
