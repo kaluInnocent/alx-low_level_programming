3703d77af24a31d9ac9719415b149b824869b43c
Innocent <kaluinnocentokoro@gmail.com>
2021-09-06 12:22:49 -0700

    vreated 0-reset_to_98.c file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - function takes a pointer to an int
+* as parameter and duplicates the value it points to 98
+* @*n: the pointer variable
+* Return: return nothing
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
