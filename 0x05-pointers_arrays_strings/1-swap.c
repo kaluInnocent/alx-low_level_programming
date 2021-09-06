fb1e53463116e527b7b4b7c9ee1a429b404f9ab4
Innocent <kaluinnocentokoro@gmail.com>
2021-09-06 12:54:17 -0700

    created 1-swap.c file

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+* swap_int - function sawps the values of two integers
+* @a: first parameter to be supplied
+* @b: second parameter to be supplied
+* Return: returns nothing
+*/
+void swap_int(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
