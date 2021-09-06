88d013343342657ce2a65c4edda243654a8e05cc
Innocent <kaluinnocentokoro@gmail.com>
2021-09-06 15:10:21 -0700

    created 5-rev_string.c file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+/**
+* rev_string - function reverses a string
+* @s: string to be reversed
+* Return: function returns nothing
+*/
+void rev_string(char *s)
+{
+	int len, temp, i;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+
+	}
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
+	}
+}
