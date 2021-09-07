96b2082b27163e81adc55f4f6883a9d315cc4de3
Innocent <kaluinnocentokoro@gmail.com>
2021-09-06 20:10:22 -0700

    updated 100-atoi.c file

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@ int _atoi(char *s)
 {
 	int i = 1;
 	unsigned int num = 0;
-	
+
 	do {
 		if (*s == '-')
 			i *= -1;
@@ -16,7 +16,6 @@ int _atoi(char *s)
 			num = num * 10 + (*s - '0');
 		else if (num > 0)
 			break;
-	}
-	while (*s++);
-	return (num *i);
+	} while (*s++);
+	return (num * 1);
 }
