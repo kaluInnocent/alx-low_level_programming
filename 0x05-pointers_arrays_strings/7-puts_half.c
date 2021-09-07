c23a4facdeb917ca1a33167efa13f824b37b7eeb
Innocent <kaluinnocentokoro@gmail.com>
2021-09-06 17:27:22 -0700

    created 7-puts_half.c file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* puts_half - function prints half of string followed by a newline;
+* function prints the second half of the string
+* if the umbe of chracters is odd,
+* function prints the last n characters of the strings.
+* @str: string to but print in half
+* Return: returns nothing
+*/
+void puts_half(char *str)
+{
+	int len, mid, i;
+
+	for (len = 0; str[len]; len++)
+	{
+
+	}
+	if (len % 2 == 0)
+		mid = len / 2;
+	else
+		mid = (len + 1) / 2;
+	for (i = mid + 1; str[i]; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
