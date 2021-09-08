e92103ec8c993863739420bed97c364c8da81d19
Innocent <kaluinnocentokoro@gmail.com>
2021-09-08 05:10:55 -0700

    created 0-strcat.c file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* main - maifunctions and header file for prototypes
+* void prototypes (void)
+* int prototypes (int)
+*/
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+#endif
