63b308049f49053ee6de98d1e867bb2ae54f9fe2
Innocent <kaluinnocentokoro@gmail.com>
2021-09-09 02:33:54 -0700

    corrected infinite looping

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 * _strncpy - function copies a string
 * @dest: string to be copied into
@@ -9,8 +10,15 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (i < n && src[i] != '\0')
+	{
 		dest[i] = src[i];
-	dest[i] = '\0';
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 	return (dest);
 }
