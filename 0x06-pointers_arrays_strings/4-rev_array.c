8e80c6ceaf0a4e260b1b6b8b96e587aa9f32f127
Innocent <kaluinnocentokoro@gmail.com>
2021-09-09 02:44:18 -0700

    added a blank line after declaration

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 * reverse_array - function reverses the content of an array
 * @a: pointer to array
@@ -11,6 +12,7 @@ void reverse_array(int *a, int n)
 	while (start < end)
 	{
 		int temp = a[start];
+
 		a[start] = a[end];
 		a[end] = temp;
 		start++;
