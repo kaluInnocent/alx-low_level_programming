c9cbd4f470c1d3333e450bb5068aa792b25412ed
Innocent <kaluinnocentokoro@gmail.com>
2021-09-13 15:03:03 -0700

    created 3-strspn.c file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -14,4 +14,5 @@ void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
 char *_strpbrk(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept);
 #endif
