878267e9f5cebabc8c44b01bb5ccc752df46db19
Innocent <kaluinnocentokoro@gmail.com>
2021-09-13 04:16:02 -0700

    created 0-memset.c file

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* main - main function's and header file for prototypes
+* void prototypes (void)
+* int prototypes (int)
+*/
+
+int _putchar(char c);
+char *_memset(char *s, char b, unsigned int n);
+#endif
