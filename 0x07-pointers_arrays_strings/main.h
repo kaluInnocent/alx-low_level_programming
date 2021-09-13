e89a040ef04cd3201b57593f64fa248765455a10
Innocent <kaluinnocentokoro@gmail.com>
2021-09-13 14:45:29 -0700

    created 4-strpbrk.c file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,4 +13,5 @@ void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 char *_strchr(char *s, char c);
 char *_strstr(char *haystack, char *needle);
+char *_strpbrk(char *s, char *accept);
 #endif
