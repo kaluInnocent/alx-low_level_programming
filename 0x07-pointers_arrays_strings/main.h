2125ddd1f98e2a9864692f4c375c1d62a6e618e8
Innocent <kaluinnocentokoro@gmail.com>
2021-09-13 10:33:54 -0700

    created 2-strchr.c file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,4 +11,5 @@ char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
+char *_strchr(char *s, char c);
 #endif
