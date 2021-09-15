a52b84a1af06a4e1be75ece7374d065874f7cb5a
Innocent <kaluinnocentokoro@gmail.com>
2021-09-15 02:25:53 -0700

    created 4-pow_recursion.c file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+/**
+* _pow_recursion - function returns the value of
+* x raised to the power of y
+* @x: is the radix or base
+* @y: is the index
+* Return; function returns the value of x to power y
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
