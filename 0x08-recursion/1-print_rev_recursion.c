99eb0d7ca51934b18ec734b518ebfdc94174d3c1
Innocent <kaluinnocentokoro@gmail.com>
2021-09-15 09:00:38 -0700

    1-print_rev_recursion.c file

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _print_rev_recursion - function prints the reverse of a string
+* @s: string to be reversed
+* Return: functions returns nothin
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+		_putchar(*s);
+}
