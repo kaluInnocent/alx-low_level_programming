c1fcab021294829ce00ed68380560118d000ee51
Innocent <kaluinnocentokoro@gmail.com>
2021-09-17 10:29:38 -0700

    created 4-add.c file

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,26 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 /**
-* is_digit - function tests for the presence 0f a digit
-* @ch: string to be checked for digit
-* Return: returns 1 if digit and 0 otherwise
-*/
-int is_digit(char *ch)
-{
-	int i, digit = 0;
-
-	for (i = 0; ch[i]; i++)
-	{
-		if (ch[i] < '0' || ch[i] > '9')
-		{
-			return (-1);
-		}
-		else
-			digit = digit * 10 + (ch[i] - '0');
-	}
-	return (digit);
-}
-/**
 * main - adds positive numbers
 * if no number is passed to the program, print-0, followed by a new line
 * if one of the number contains symbols that are not digits, print Error,
@@ -32,25 +12,20 @@ int is_digit(char *ch)
 */
 int main(int argc, char *argv[])
 {
-	int i, digit, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc > 0)
+	for (i = 1; i < argc; i++)
 	{
-		if (argc == 1)
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("0\n");
-		}
-		for (i = 0; i < argc; i++)
-		{
-			digit = is_digit(argv[i]);
-			if (digit == -1)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 			{
 				printf("Error\n");
 				return (1);
 			}
-			sum += digit;
 		}
-		printf("%d\n", sum);
+		sum += atoi(argv[i]);
 	}
+	printf("%d\n", sum);
 	return (0);
 }
