98658fc96d64950c0997d6363130f89206aa7078
Innocent Kalu <kaluinnocentokoro@gmail.com>
2021-09-17 02:28:46 +0100

    created a README file

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+* main - main function's and header file for prototypes
+* void prototypes (void)
+* int prototypes (int)
+*/
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
