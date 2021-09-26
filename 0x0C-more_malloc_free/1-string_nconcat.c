136ab883f5f8760bc5937449fa2d071d7f406826
Innocent <kaluinnocentokoro@gmail.com>
2021-09-26 11:49:11 -0700

    updated advanced tasks

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,44 @@
 #include "main.h"
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+
 /**
-* string_nconcat - fucntion concatenates two strings
-* @s1: first string to be concatenated
-* @s2: second string to be concatenated
-* @n: number of characters of s2 to be concatenated with s1
-* Return: returns a pointer to a newly allocated space in memory
-* which contains s1 followed by the first n bytes of s2 and null terminated
-* if the function fails, it should return NULL
-* if n is greater or equal to lenght of s2 we concatenate entire s2
-* if NULL is passed, treat it as empty string
+* string_nconcat - Concatenates two strings using at
+*                  most an inputted number of bytes.
+* @s1: The first string.
+* @s2: The second string.
+* @n: The maximum number of bytes of s2 to concatenate to s1.
+*
+* Return: If the function fails - NULL.
+*         Otherwise - a pointer to the concatenated space in memory.
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int len1 = 0, len2 = 0, i, j;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0; s1[i]; i++)
-		len1++;
-	for (j = 0; s2[j]; j++)
-		len2++;
-	str = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (str == NULL)
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
 		return (NULL);
-	for (i = 0; s1[i]; i++)
-		*(str + i) = *(s1 + i);
-	if (n >= len2)
-		n = len2;
-	for (j = 0; j < n; j++)
-		str[i++] = s2[j];
-	str[i] = '\0';
-	return (str);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,60 +1,55 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * _memcpy - copy memory data from src to dest
- * @dest: memory destination
- * @src: memory source
- * @n: size of new memory
+ * _realloc - Reallocates a memory block using malloc and free.
+ * @ptr: A pointer to the memory previously allocated.
+ * @old_size: The size in bytes of the allocated space for ptr.
+ * @new_size: The size in bytes for the new memory block.
+ *
+ * Return: If new_size == old_size - ptr.
+ *         If new_size == 0 and ptr is not NULL - NULL.
+ *         Otherwise - a pointer to the reallocated memory block.
  */
-void _memcpy(void *dest, void *src, size_t n)
-{
-	size_t i;
-	char *csrc = (char *)src;
-	char *cdest = (char *)dest;
-
-	for (i = 0; i < n; i++)
-		cdest[i] = csrc[i];
-}
-
-/**
-* _realloc - function reallocates a memory block using malloc and free
-* void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-* _realloc - Reallocates a memory block using malloc and free.
-* @ptr: A pointer to the memory previously allocated.
-* @old_size: The size in bytes of the allocated space for ptr.
-* @new_size: The size in bytes for the new memory block.
-*
-* Return: If new_size == old_size - ptr.
-* If new_size == 0 and ptr is not NULL - NULL.
-* Otherwise - a pointer to the reallocated memory block.
-*/
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *newPtr;
+	void *mem;
+	char *ptr_copy, *filler;
+	unsigned int index;
 
-	if (new_size == 0)
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
 	{
-		if (ptr != NULL)
-			free(ptr);
+		mem = malloc(new_size);
+
+		if (mem == NULL)
+			return (NULL);
+
+		return (mem);
+	}
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
 		return (NULL);
 	}
-	else if (!ptr)
+
+	ptr_copy = ptr;
+	mem = malloc(sizeof(*ptr_copy) * new_size);
+
+	if (mem == NULL)
 	{
-		return (malloc(new_size));
+		free(ptr);
+		return (NULL);
 	}
-	else if (new_size <= old_size)
-	{
-		return (ptr);
-	}
-	else
-	{
-		newPtr = malloc(new_size);
-		if (newPtr)
-		{
-			_memcpy(newPtr, ptr, old_size);
-			free(ptr);
-		}
-		return (newPtr);
-	}
-	return (0);
+
+	filler = mem;
+
+	for (index = 0; index < old_size && index < new_size; index++)
+		filler[index] = *ptr_copy++;
+
+	free(ptr);
+	return (mem);
 }
