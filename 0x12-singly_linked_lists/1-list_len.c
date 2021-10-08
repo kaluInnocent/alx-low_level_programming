3b794f985addab59e82cb74dc9d2521569b0e044
Innocent <kaluinnocentokoro@gmail.com>
2021-10-08 05:12:56 -0700

    created 1-list_len.c file

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* print_list - function prints all elements of a list
+* @h: node head
+* Return: the number of nodes in the head
+*/
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	while (h != NULL)
+	{
+		node_count++;
+		h = h->next;
+	}
+	return (node_count);
+}
