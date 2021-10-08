c148ae45169ae0297e0723a99cc11a704bad834c
Innocent <kaluinnocentokoro@gmail.com>
2021-10-08 04:58:07 -0700

    updated 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,13 +14,11 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
-		{
-			printf("[%d] %s", h->len, h->str);
-			node_count++;
-			h = h->next;
-		}
+			printf("[%d] %s\n", h->len, h->str);
+		node_count++;
+		h = h->next;
 	}
 	return (node_count);
 }
