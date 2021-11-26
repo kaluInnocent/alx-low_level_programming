91989ffc1144ac923a70991552828d68d6146ba7
Innocent <kaluinnocentokoro@gmail.com>
2021-11-26 13:25:13 -0800

    compledted task 0: Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+* print_dlistint - function prints the elements of a doubly linked list
+* @h: pointer to the start of the doubly linked list
+* Return: number of nodes in the list
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+	if (h == NULL)
+	{
+		return 0;
+	}
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
