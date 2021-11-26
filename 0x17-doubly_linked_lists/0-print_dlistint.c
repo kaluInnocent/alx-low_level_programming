30a123d41368996c2699a219c2f643d216636cee
Innocent <kaluinnocentokoro@gmail.com>
2021-11-26 13:35:46 -0800

    updated code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,7 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
 * print_dlistint - function prints the elements of a doubly linked list
@@ -9,17 +12,18 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
+	const dlistint_t *temp = h;
 
 	if (h == NULL)
 	{
 		return (0);
 	}
 
-	while (h)
+	while (temp != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%d\n", temp->n);
 		count++;
-		h = h->next;
+		temp = temp->next;
 	}
 	return (count);
 }
