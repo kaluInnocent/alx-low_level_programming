2e8e5a5968933a9a6593299069c16b6f71467e74
Innocent <kaluinnocentokoro@gmail.com>
2021-11-28 10:09:54 -0800

    added a return value in function description

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -5,6 +5,7 @@
 * @h: pointer to a pointer to the start of the list
 * @idx: index of the list where the new node should be added
 * @n: data to be inserted at the given position
+* Return: The address of the new node or NULL if it failed
 */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
