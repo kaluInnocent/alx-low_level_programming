440407b25e8222fddcfc6c47f8eb270fe0e9881f
Innocent <kaluinnocentokoro@gmail.com>
2021-11-29 03:06:02 -0800

    added calc.h file

---
+++ 0x18-dynamic_libraries/calc.h
@@ -0,0 +1,13 @@
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
