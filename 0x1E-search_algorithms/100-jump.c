01144c73cba8e1896d49d59d1f1e5d20c12772e7
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-25 19:55:06 +0100

    added tadk 7

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include<math.h>
+#include "search_algos.h"
+
+/**
+* jump_search - function performs a jump search
+* @array: List to be searched
+* @size: size of array
+* @value: value to be searched in array
+* Return: index of value if fpunf, -1 otherwise
+*/
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t block = sqrt(size);
+
+	if (array)
+	{
+		while (array[block] <= value && block < size)
+		{
+			i = block;
+			block += sqrt(size);
+			if (block > size - 1)
+				return (-1);
+		}
+		int j;
+		for (j = i; j < block; j++)
+		{
+			printf("Value checked array[%ld] = [%d]\n", j, array[j]);
+			if (array[j] == value)
+				return (j);
+		}
+	}
+	return (-1);
+}
