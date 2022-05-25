dfe1ceb0b45f2de8afd559e860f3bdbd67fd8671
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-25 17:08:21 +0100

    add task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,36 @@
+#include "search_algos.h"
+
+/**
+* binary_search - function performs a binary search
+* @array: The array to be searched
+* @size: size of the array
+* @value: value to be searched in the array
+* Return: index of value if found, -1 otherwise
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, low, high;
+
+	if (array)
+	{
+		for (low = 0, high = size - 1; high >= low;)
+		{
+			printf("Searching in array: ");
+			i = low;
+			while (i < high)
+			{
+				printf("%d\n", array[i]);
+				i++;
+			}
+			printf("%d\n", array[i]);
+			i = low + (high - low) / 2;
+			if (array[i] == value)
+				return (i);
+			if (array[i] > value)
+				high = i - 1;
+			else
+				low = i + 1;
+		}
+	}
+	return (-1);
+}
