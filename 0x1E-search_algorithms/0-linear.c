f67997adcbc1bacf6445d5a00cd02c62afada2d5
Innocent Kalu <obinnasmartkalu@gmail.com>
2022-05-25 13:33:10 +0100

    updated task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,14 +20,10 @@ int linear_search(int *array, size_t size, int value)
 	{
 		for (i = 0; i < size; i++)
 		{
-			printf("Value checked array[%d] = [%d]\n", i, array[i]);
+			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
 			{
 				return (i);
-			}
-			else if (i + 1 == size)
-			{
-				return (-1);
 			}
 		}
 	}
